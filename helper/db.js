83902dc62a590df5d27bf2e24ec40d1921e263f1
Eray Kısabacak <nefret19981998@hotmail.com>
2019-02-07 23:51:15 +0300

    Kayıt Ekleme Yapıldı

---
+++ helper/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb://movie_user:abcd1234@ds217138.mlab.com:17138/movie-api',{  useNewUrlParser: true});
+    mongoose.connection.on('open', () => {
+        console.log('MongoDB: Connected');
+    });
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB: Error', err);
+    });
+
+    mongoose.Promise = global.Promise;
+
+};
