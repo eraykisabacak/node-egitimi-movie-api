e1d8e8108ae78df799063b7251a602260ac84fba
Eray Kısabacak <nefret19981998@hotmail.com>
2019-02-11 22:52:57 +0300

    movie and director validasyon

--- models/Movie.js
+++ models/Movie.js
@@ -4,13 +4,31 @@ const Schema = mongoose.Schema;
 const MovieSchema = new Schema({
    director_id: Schema.Types.ObjectId,
    title:{
-       type: String,
-       required: true
+     type: String,
+     required: [true, '`{PATH}` alanı zorunludur'],
+     maxlength: [10, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) değerinden fazla olamaz '],
+     minlength: [2, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) değerinden az olamaz ']
    },
-   category: String,
-   country: String,
-   year: Number,
-   imdb_score: Number,
+   category: {
+       type: String,
+       maxlength: [30,'`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) değerinden fazla olamaz '],
+       minlength: [5, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) değerinden az olamaz ']
+   },
+   country: {
+       type: String,
+       maxlength: [30,'`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) değerinden fazla olamaz '],
+       minlength: [5, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) değerinden az olamaz ']
+   },
+   year: {
+       type: Number,
+       maxlength: 2040,
+       minlength: 1900
+   },
+   imdb_score: {
+       type: Number,
+       maxlength:10,
+       minlength:0
+   },
    createdAt:{
        type:Date,
        default: Date.now
