cd447500a174155a0b24052f5b60d2bdbc62ad8d
Eray Kısabacak <nefret19981998@hotmail.com>
2019-02-21 11:51:34 +0300

    Movie Test Finally

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -29,7 +29,6 @@ describe('/api/movies tests',() => {
                });
         })
     });
-
     describe('/POST movie', () => {
         it('it should POST a movie', (done) => {
             const movie = {
@@ -59,8 +58,7 @@ describe('/api/movies tests',() => {
                 });
         });
     });
-
-    describe('/GET/:director_id movie', () => {
+    describe('/GET/:movie_id movie', () => {
         it('it should GET a movie by the given id', (done) => {
 
 
@@ -81,6 +79,48 @@ describe('/api/movies tests',() => {
                });
         });
     })
+    describe('/PUT/:movie_id movie', () => {
+        it('it should UPDATE a movie given by id', (done) => {
+            const movie = {
+                title: '93Creative',
+                director_id: '5c6139027dbc80571409bf56',
+                category: 'Suç',
+                country: 'Fransa',
+                year: 1970,
+                imdb_score:9
+            };
+
+            chai.request(server)
+                .put('/api/movies/'+movieId)
+                .send(movie)
+                .set('x-access-token',token)
+                .end((err,res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title').eql(movie.title);
+                    res.body.should.have.property('director_id').eql(movie.director_id);
+                    res.body.should.have.property('category').eql(movie.category);
+                    res.body.should.have.property('country').eql(movie.country);
+                    res.body.should.have.property('year').eql(movie.year);
+                    res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+                    done();
+                });
+        });
+    });
+    describe('/DELETE/:movie_id movie', () => {
+        it('it should DELETE a movie given by id', (done) => {
+            chai.request(server)
+                .delete('/api/movies/'+movieId)
+                .set('x-access-token',token)
+                .end((err,res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('status').eql(1);
+                    done();
+                });
+        });
+    });
+
 });
 
 
